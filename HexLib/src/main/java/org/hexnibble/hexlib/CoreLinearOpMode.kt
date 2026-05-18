90bd2adaa22d01a716a90c18a8ba8a55120765f8
53142 <89957019+53142@users.noreply.github.com>
2026-05-18 09:43:28 -0700

    initial files for lib and tele

---
+++ HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
@@ -0,0 +1,9 @@
+package org.hexnibble.hexlib
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+class CoreLinearOpMode : LinearOpMode() {
+    override fun runOpMode() {
+        TODO("Not yet implemented")
+    }
+}

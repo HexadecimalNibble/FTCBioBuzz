d47425022d4f371e4d1d1bda484ef3565cb4f807
53142 <89957019+53142@users.noreply.github.com>
2026-05-20 15:45:30 -0700

    Fix not compiling

--- HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
+++ HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
@@ -1,31 +1,31 @@
-package org.hexnibble.hexlib
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-
-class CoreLinearOpMode : LinearOpMode() {
-    private val logTag = "CoreLinearOpMode"
-    val rcController = RCController()
-    override fun runOpMode() {
-        L.d(logTag, "Initializing OpMode")
-        onPressInit()
-        // Play pressed
-        if (opModeIsActive()) {
-            onPressPlay()
-
-            while (!isStopRequested && !opModeComplete) {
-                // Write sensor data to shared memory
-                mainLoopWriteSensorsToSharedMemory()
-
-                runOpModeLoop(rcController, CoreLinearOpMode.robot)
-                onLoopCustom()
-
-                // Read sensor data back from shared memory
-                CoreLinearOpMode.robot.readDataFromSharedMemory()
-                CoreLinearOpMode.robot.sendCommandsToDevices()
-
-                processTelemetry()
-                getOpModeTimer().processLoopTime()
-            }
-        }
-    }
-}
+//package org.hexnibble.hexlib
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+//
+//class CoreLinearOpMode : LinearOpMode() {
+//    private val logTag = "CoreLinearOpMode"
+//    val rcController = RCController()
+//    override fun runOpMode() {
+//        L.d(logTag, "Initializing OpMode")
+//        onPressInit()
+//        // Play pressed
+//        if (opModeIsActive()) {
+//            onPressPlay()
+//
+//            while (!isStopRequested && !opModeComplete) {
+//                // Write sensor data to shared memory
+//                mainLoopWriteSensorsToSharedMemory()
+//
+//                runOpModeLoop(rcController, CoreLinearOpMode.robot)
+//                onLoopCustom()
+//
+//                // Read sensor data back from shared memory
+//                CoreLinearOpMode.robot.readDataFromSharedMemory()
+//                CoreLinearOpMode.robot.sendCommandsToDevices()
+//
+//                processTelemetry()
+//                getOpModeTimer().processLoopTime()
+//            }
+//        }
+//    }
+//}

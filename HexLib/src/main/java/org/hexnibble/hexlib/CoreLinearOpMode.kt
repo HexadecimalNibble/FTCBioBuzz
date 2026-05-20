5ebe1c1ea5f126b836de2930cd0b2cf5109011a7
53142 <89957019+53142@users.noreply.github.com>
2026-05-20 08:19:28 -0700

    changes

--- HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
+++ HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
@@ -3,7 +3,10 @@ package org.hexnibble.hexlib
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 
 class CoreLinearOpMode : LinearOpMode() {
+    private val logTag = "CoreLinearOpMode"
+    val rcController = RCController()
     override fun runOpMode() {
-        TODO("Not yet implemented")
+        L.d(logTag, "Initializing OpMode")
+        onPressInit()
     }
 }
---
+++ HexLib/src/main/java/org/hexnibble/hexlib/RCController.kt
@@ -0,0 +1,4 @@
+package org.hexnibble.hexlib
+
+class RCController {
+}

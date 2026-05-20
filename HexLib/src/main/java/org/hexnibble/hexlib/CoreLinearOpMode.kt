1fd3d15d4e9a5f355ae82b19f7772b78bfda7bb6
53142 <89957019+53142@users.noreply.github.com>
2026-05-20 10:51:04 -0700

    changes

--- HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
+++ HexLib/src/main/java/org/hexnibble/hexlib/CoreLinearOpMode.kt
@@ -8,5 +8,24 @@ class CoreLinearOpMode : LinearOpMode() {
     override fun runOpMode() {
         L.d(logTag, "Initializing OpMode")
         onPressInit()
+        // Play pressed
+        if (opModeIsActive()) {
+            onPressPlay()
+
+            while (!isStopRequested && !opModeComplete) {
+                // Write sensor data to shared memory
+                mainLoopWriteSensorsToSharedMemory()
+
+                runOpModeLoop(rcController, CoreLinearOpMode.robot)
+                onLoopCustom()
+
+                // Read sensor data back from shared memory
+                CoreLinearOpMode.robot.readDataFromSharedMemory()
+                CoreLinearOpMode.robot.sendCommandsToDevices()
+
+                processTelemetry()
+                getOpModeTimer().processLoopTime()
+            }
+        }
     }
 }

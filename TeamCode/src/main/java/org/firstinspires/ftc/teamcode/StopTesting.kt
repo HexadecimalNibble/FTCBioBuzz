08214cd57d31656d46a55ab0913ff859d9c7f6bc
53142 <89957019+53142@users.noreply.github.com>
2026-06-12 09:53:40 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StopTesting.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StopTesting.kt
@@ -1,55 +1,41 @@
-//package org.firstinspires.ftc.teamcode
-//
-//import com.qualcomm.hardware.lynx.LynxModule
-//import com.qualcomm.hardware.lynx.LynxUsbDevice
-//import com.qualcomm.hardware.lynx.commands.LynxDatagram
-//import com.qualcomm.hardware.lynx.commands.core.LynxSetMotorConstantPowerCommand
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-//import com.qualcomm.robotcore.hardware.usb.serial.RobotUsbDeviceTty
-//import com.qualcomm.robotcore.hardware.usb.serial.SerialPort
-//import org.firstinspires.ftc.robotcore.internal.system.AppUtil
-//import org.firstinspires.ftc.robotcore.internal.ui.UILocation
-//import org.hexnibble.hexlib.L
-//import java.io.FileInputStream
-//import java.io.FileOutputStream
-//import java.lang.reflect.Field
-//import java.lang.reflect.Modifier
-//
-//@TeleOp
-//class StopTesting : LinearOpMode() {
-//  init {
-//    AppUtil.getInstance().showToast(UILocation.BOTH, "Hi", 1)
-////    AppUtil.getInstance().showAlertDialog(UILocation.BOTH, "ALERT TITLE!!!", "COOL ALERT MESSAGE!")
-//  }
-//  override fun runOpMode() {
-//    val controlHub: LynxModule = hardwareMap.get(LynxModule::class.java, "Control Hub")
-//
-//    val usbDevField = LynxModule::class.java.getDeclaredField("lynxUsbDevice").also { it.isAccessible = true }
-//    val usbDev = usbDevField.get(controlHub) as LynxUsbDevice
-//
-//    val serialPortField = RobotUsbDeviceTty::class.java.getDeclaredField("serialPort").also { it.isAccessible = true }
-//    val port = serialPortField.get(usbDev.robotUsbDevice) as SerialPort
-//
-//    val inputStream = port.inputStream
-//    val outputStream = port.outputStream
-//
-//    val cmd = LynxSetMotorConstantPowerCommand(controlHub, 0, 32767)
-//    val msgNumFunction = LynxModule::class.java.getDeclaredMethod("getNewMessageNumber").also { it.isAccessible = true }
-//    cmd.messageNumber = (msgNumFunction(controlHub) as Byte).toInt()
-//
-//    val serialization = LynxDatagram(cmd).toByteArray()
-//
-//    waitForStart()
-//    while (opModeIsActive()) {
-//      L.i("StopTesting", "running!")
-//      if (gamepad1.crossWasPressed()) {
-//        outputStream.write(serialization)
-//        inputStream.read()
-//      }
-//    }
-//    while (true) {
-//      L.i("StopTesting", "Stopped but still running!")
-//    }
-//  }
-//}
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotorImplEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+import org.firstinspires.ftc.robotcore.internal.ui.UILocation
+import kotlin.math.abs
+
+@TeleOp
+class StopTesting : LinearOpMode() {
+  init {
+    AppUtil.getInstance().showToast(UILocation.BOTH, "Hi", 1)
+  }
+  override fun runOpMode() {
+    val LFMotor = hardwareMap.get(DcMotorImplEx::class.java, "LFMotor").also { it.direction = DcMotorSimple.Direction.REVERSE }
+    val LBMotor = hardwareMap.get(DcMotorImplEx::class.java, "LBMotor").also { it.direction = DcMotorSimple.Direction.REVERSE }
+    val RFMotor = hardwareMap.get(DcMotorImplEx::class.java, "RFMotor")
+    val RBMotor = hardwareMap.get(DcMotorImplEx::class.java, "RBMotor")
+
+    waitForStart()
+    while (opModeIsActive()) {
+      val robotX = gamepad1.left_stick_x
+      val robotY = -gamepad1.left_stick_y
+      val triggers = gamepad1.right_trigger - gamepad1.left_trigger
+
+      val lFRaw = robotY + robotX + triggers
+      val lBRaw = robotY - robotX + triggers
+      val rFRaw = robotY - robotX - triggers
+      val rBRaw = robotY + robotX - triggers
+
+      val denom = maxOf(abs(lFRaw), abs(lBRaw), abs(rFRaw), abs(rBRaw), 1f)
+
+      val lFMotor = lFRaw / denom
+      val lBMotor = lBRaw / denom
+      val rFMotor = rFRaw / denom
+      val rBMotor = rBRaw / denom
+    }
+  }
+}

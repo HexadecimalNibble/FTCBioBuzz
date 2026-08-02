bc580db3785ee4321a931dd6e5e242b3e900c930
53142 <89957019+53142@users.noreply.github.com>
2026-08-02 11:27:36 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.kt
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
+import dev.nextftc.robot.opmode.BulkReadHook
 import dev.nextftc.robot.opmode.NextOpMode
 import org.firstinspires.ftc.teamcode.robot.BiobuzzRobot
 
-class Teleop(robot: BiobuzzRobot) : NextOpMode(robot) {
+class Teleop(robot: BiobuzzRobot) : NextOpMode(robot, BulkReadHook) {
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Intake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Intake.kt
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.robot
+import dev.nextftc.hardware.actuators.NextFeedbackServo
 import dev.nextftc.hardware.actuators.NextMotor
 import dev.nextftc.robot.Mechanism
 
 class Intake : Mechanism {
   val intakeMotor = NextMotor("intakeMotor")
+
+  val servo = NextFeedbackServo()
 
   fun startIntake() = instant { intakeMotor.throttle = 1.0 }
   fun stopIntake() = instant { intakeMotor.throttle = 0.0 }

2dc40059de7bb36af0a2b2108cc4c16b09529d87
53142 <89957019+53142@users.noreply.github.com>
2026-08-09 08:48:18 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Intake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Intake.kt
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode.robot
 import dev.nextftc.hardware.actuators.NextFeedbackServo
 import dev.nextftc.hardware.actuators.NextMotor
+import dev.nextftc.hardware.sensors.NextAnalogInput
 import dev.nextftc.robot.Mechanism
 
 class Intake : Mechanism {
   val intakeMotor = NextMotor("intakeMotor")
 
-//  val servo = NextFeedbackServo()
+  val servoAnalogInput = NextAnalogInput("servoAnalog")
+  val servo = NextFeedbackServo("servo", servoAnalogInput)
+
+  fun moveServo() = instant { servo.position = 0.3 }
 
   fun startIntake() = instant { intakeMotor.throttle = 1.0 }
   fun stopIntake() = instant { intakeMotor.throttle = 0.0 }

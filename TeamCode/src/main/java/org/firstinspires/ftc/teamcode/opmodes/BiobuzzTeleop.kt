2e5f5edf48c838777de97bc6e0adad3018dd2615
53142 <89957019+53142@users.noreply.github.com>
2026-09-05 16:12:25 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BiobuzzTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BiobuzzTeleop.kt
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.opmodes
 
 import dev.nextftc.robot.opmode.BulkReadHook
 import dev.nextftc.robot.opmode.NextOpMode
+import dev.nextftc.robot.opmode.NextTeleop
 import org.firstinspires.ftc.teamcode.robot.BiobuzzRobot
 
-class Teleop(robot: BiobuzzRobot) : NextOpMode(robot, BulkReadHook) {
+@NextTeleop(name = "Biobuzz Teleop")
+class BiobuzzTeleop(robot: BiobuzzRobot) : NextOpMode(robot, BulkReadHook) {
 }
